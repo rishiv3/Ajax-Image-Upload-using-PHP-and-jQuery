cbeed0c620df6e70d7823c6f2417996866e94284
Rishi Vishwakarma <rishivishwakarma24x7@gmail.com>
2015-11-19 11:12:22 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+		<meta name="robots" content="noindex, nofollow">
+        <title>Ajax Image Upload Using PHP and jQuery</title>
+        <link rel="stylesheet" href="style.css" />
+		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="script.js"></script>
+    </head>
+    <body> 
+		<div class="main">
+        <h1>Ajax Image Upload</h1><br/>
+		<hr>
+            <form id="uploadimage" action="" method="post" enctype="multipart/form-data">
+				<div id="image_preview"><img id="previewing" src="noimage.png" /></div>	
+        <hr id="line">    
+			<div id="selectImage">
+				<label>Select Your Image</label><br/>
+                <input type="file" name="file" id="file" required />
+				<input type="submit" value="Upload" class="submit" />
+            </div>                   
+			</form>		
+		</div> 
+		<h4 id='loading' ><img src="loading_circle.gif"/>&nbsp;&nbsp;Loading...</h4>
+		<div id="message"> 			
+        </div>
+    </body>
+</html>           
+              
+             
+                     
+             
+           
+
