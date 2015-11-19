e45b0a614c557bf009acd749a62d58109e414966
Rishi Vishwakarma <rishi24x7@rocketmail.com>
2015-11-19 12:01:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-		<meta name="robots" content="noindex, nofollow">
+	<meta name="robots" content="noindex, nofollow">
         <title>Ajax Image Upload Using PHP and jQuery</title>
         <link rel="stylesheet" href="css/style.css" />
 		<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
@@ -8,27 +8,20 @@
         <script src="js/script.js"></script>
     </head>
     <body> 
-		<div class="main">
+	<div class="main">
         <h1>Ajax Image Upload</h1><br/>
 		<hr>
-            <form id="uploadimage" action="" method="post" enctype="multipart/form-data">
-				<div id="image_preview"><img id="previewing" src="images/noimage.png" /></div>	
-        <hr id="line">    
-			<div id="selectImage">
-				<label>Select Your Image</label><br/>
-                <input type="file" name="file" id="file" required />
-				<input type="submit" value="Upload" class="submit" />
-            </div>                   
-			</form>		
-		</div> 
-		<h4 id='loading' ><img src="images/loading_circle.gif"/>&nbsp;&nbsp;Loading...</h4>
-		<div id="message"> 			
-        </div>
+	        <form id="uploadimage" action="" method="post" enctype="multipart/form-data">
+			<div id="image_preview"><img id="previewing" src="images/noimage.png" /></div>	
+	        	<hr id="line">    
+				<div id="selectImage">
+					<label>Select Your Image</label><br/>
+	                		<input type="file" name="file" id="file" required />
+					<input type="submit" value="Upload" class="submit" />
+	            		</div>                   
+		</form>		
+	</div> 
+	<h4 id='loading' ><img src="images/loading_circle.gif"/>&nbsp;&nbsp;Loading...</h4>
+	<div id="message"></div>
     </body>
-</html>           
-              
-             
-                     
-             
-           
-
+</html>
